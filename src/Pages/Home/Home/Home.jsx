226500c79cac534a71dccddca28a0d3a8c23112a
rebahaider <rebahaiderema@gmail.com>
2024-05-19 00:18:29 +0600

    added banner section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h1 className="text-3xl">this is from home</h1>
+            <Banner></Banner>
         </div>
     );
 };
